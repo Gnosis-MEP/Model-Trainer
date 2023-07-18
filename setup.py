cfe6fbbc66185ab0f1f4dd87dafdd64f470dae32
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2023-07-18 11:51:45 +0100

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='model_trainer',
+    version='0.1',
+    description='A service that provide training for some types of models',
+    author='Felipe Arruda Pontes',
+    author_email='felipe.arruda.pontes@insight-centre.org',
+    packages=['model_trainer'],
+    zip_safe=False
+)
